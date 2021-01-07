8bdda2a4e32bc8507d3d26f1685d34271828baa5
Rob Bos <raj.bos@gmail.com>
2021-01-07 18:09:10 +0100

    wrong command

--- src/empty.ps1
+++ src/empty.ps1
@@ -1,8 +1,8 @@
 Write-Host "Welcome to the dependency updater"
 
 # search for the location of the start file
-Get-Children "start.ps1" -Recurse
+Get-Childitem "start.ps1" -Recurse
 
 cd ~
 
-Get-Children "start.ps1" -Recurse
+Get-Childitem "start.ps1" -Recurse
